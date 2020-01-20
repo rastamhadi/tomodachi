005c57ee8c77a81fbf76660bf4841be4dc41c139
Rastam Hadi <rastam.hadi@gmail.com>
2020-01-21 01:33:32 +0900

    Get tests working

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
+    @user = User.create(name: 'Monica')
   end
 
   test "should get index" do
