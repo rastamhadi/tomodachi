1582ce4fa19bcc453e1399f43057cdff8b11dc96
Rastam Hadi <rastam.hadi@gmail.com>
2020-01-21 02:33:28 +0900

    Clean database before each test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = User.create(name: 'Monica')
-  end
-
   test "should get index" do
     get users_url
     assert_response :success
@@ -17,28 +13,36 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { name: @user.name } }
+      post users_url, params: { user: { name: 'Monica' } }
     end
 
     assert_redirected_to user_url(User.last)
   end
 
   test "should show user" do
+    @user = User.create(name: 'Rachel')
+
     get user_url(@user)
     assert_response :success
   end
 
   test "should get edit" do
+    @user = User.create(name: 'Ross')
+
     get edit_user_url(@user)
     assert_response :success
   end
 
   test "should update user" do
+    @user = User.create(name: 'Chandler')
+
     patch user_url(@user), params: { user: { name: @user.name } }
     assert_redirected_to user_url(@user)
   end
 
   test "should destroy user" do
+    @user = User.create(name: 'Joey')
+
     assert_difference('User.count', -1) do
       delete user_url(@user)
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  def after_teardown
+    Neo4j::ActiveBase.current_session.query('MATCH (n) DETACH DELETE n')
+    super
+  end
 end
