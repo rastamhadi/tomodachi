e536c4491923d8fe481d9dac85c747ff4a12e179
Rastam Hadi <rastam.hadi@gmail.com>
2020-03-11 21:41:31 +0900

    rails app:update

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,8 @@ Bundler.require(*Rails.groups)
 
 module Tomodachi
   class Application < Rails::Application
-    
     config.generators do |g|
-      g.orm             :neo4j
+      g.orm :neo4j
     end
 
     # Configure where to connect to the Neo4j DB
@@ -25,12 +24,12 @@ module Tomodachi
     # config.neo4j.session.type = :embedded
     # config.neo4j.session.path = Rails.root.join('neo4j-db').to_s
 
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
