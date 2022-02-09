cebfc7897bc9c20d250f777e0a79debe00913990
Rastam Hadi <rastam.hadi@gmail.com>
2022-02-09 21:56:33 +0900

    rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
-require 'neo4j/railtie'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,27 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Tomodachi
   class Application < Rails::Application
-    config.generators do |g|
-      g.orm :neo4j
-    end
-
-    # Configure where to connect to the Neo4j DB
-    # Note that embedded db is only available for JRuby
-    # config.neo4j.session.type = :http
-    # config.neo4j.session.url = 'http://localhost:7474'
-    #  or
-    # config.neo4j.session.type = :bolt
-    # config.neo4j.session.url = 'bolt://localhost:7687'
-    #  or
-    # config.neo4j.session.type = :embedded
-    # config.neo4j.session.path = Rails.root.join('neo4j-db').to_s
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
